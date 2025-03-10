72849e8755e41bf713a8590e2c142733014e60e0
Ansh-Palekar <anshpalekar1803@gmail.com>
2025-03-10 16:24:46 +0530

    Student Form Done Along with Hooks

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -8,14 +8,17 @@ function HomePage() {
         <h1>Welcome to the Portal</h1>
         <p>Select your role to continue</p>
         <div className="login-options">
+
           <div className="student-link">
             <Link to="/student_login">Student Login</Link>
           </div>
           <div className="teacher-link">
             <Link to="/teacher_login">Teacher Login</Link>
           </div>
+
         </div>
       </div>
+      
     </div>
   );
 }
--- src/pages/StudentLogin.jsx
+++ src/pages/StudentLogin.jsx
@@ -1,13 +1,50 @@
+import "../css/Student_Login.css";
+import { useState } from "react";
+function StudentLogin() {
+
+    const [formData,setFormData]=useState({
+        prn:"",
+        password:""
+    })
+
+    function handleChange(e)
+    {
+        const {name,value}=e.target;
+
+        setFormData((prev)=>(
+            {...prev,[name]:value}
+        ))
 
 
-function StudentLogin()
-{
-    return(
-        <div>
-               This is Student Login 
+    }
+
+    function handleSubmit(e)
+    {
+        e.preventDefault()
+
+        console.log("Form Submitted",formData)
+
+
+    }
+
+
+
+    return (
+        <div className="main">
+            <div className="form-container">
+                <h2>Student Login</h2>
+                <form onSubmit={handleSubmit}>
+                    <label>Enter Your PRN</label>
+                    <input type="text" name="prn" placeholder="Enter PRN" value={formData.prn} onChange={handleChange} required />
+
+                    <label>Enter Your Password</label>
+                    <input type="password" name="password" placeholder="Enter Password" value={formData.password} onChange={handleChange} required /><br></br>
+                    <button type="submit">Login</button>
+                
+                </form>
+            </div>
         </div>
-    )
+    );
 }
 
 export default StudentLogin;
-
