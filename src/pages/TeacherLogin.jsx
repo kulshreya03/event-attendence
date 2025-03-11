a3339e325c7c348e7fa1911892f793f8f5ced966
Ansh-Palekar <anshpalekar1803@gmail.com>
2025-03-11 17:40:07 +0530

    Teacher Login Done

--- src/pages/TeacherLogin.jsx
+++ src/pages/TeacherLogin.jsx
@@ -1,12 +1,51 @@
+import "../css/Teacher_Login.css";
+import { useState } from "react";
 
-function TeacherLogin()
-{
-    return(
-        <div>
-               This is Teacher Login 
+function TeacherLogin() {
 
+    const [formData,setFormData]=useState({
+        username:"",
+        password:""
+    })
+
+    function handleChange(e)
+    {
+        const {name,value}=e.target;
+
+        setFormData((prev)=>(
+            {...prev,[name]:value}
+        ))
+
+
+    }
+
+    function handleSubmit(e)
+    {
+        e.preventDefault()
+
+        console.log("Form Submitted",formData)
+
+
+    }
+
+
+
+    return (
+        <div className="main">
+            <div className="form-container">
+                <h2>Student Login</h2>
+                <form onSubmit={handleSubmit}>
+                    <label>Enter Your Username</label>
+                    <input type="text" name="username" placeholder="Enter Username" value={formData.username} onChange={handleChange} required />
+
+                    <label>Enter Your Password</label>
+                    <input type="password" name="password" placeholder="Enter Password" value={formData.password} onChange={handleChange} required /><br></br>
+                    <button type="submit">Login</button>
+                
+                </form>
+            </div>
         </div>
-    )
+    );
 }
 
 export default TeacherLogin;
